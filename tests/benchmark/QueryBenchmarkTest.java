97447b98e1d7b9498b17476eeb74f97d32927092
Maxime <contact@maximethebault.me>
2015-03-27 20:43:35 +0100

    Tests

--- tests/benchmark/QueryBenchmarkTest.java
+++ tests/benchmark/QueryBenchmarkTest.java
@@ -14,6 +14,7 @@ public class QueryBenchmarkTest {
         expectedSearchResults.addHit(1, 3);
         expectedSearchResults.addHit(1, 4);
         expectedSearchResults.addHit(1, 5);
+        expectedSearchResults.addHit(1, 6);
         SearchResults actualSearchResults = new SearchResults();
         actualSearchResults.addHit(1, 1);
         actualSearchResults.addHit(1, 2);
@@ -28,7 +29,34 @@ public class QueryBenchmarkTest {
         QueryBenchmark queryBenchmark = new QueryBenchmark();
         queryBenchmark.setExpectedSearchResults(expectedSearchResults);
         queryBenchmark.setActualSearchResults(actualSearchResults);
-        assertEquals(0.5, queryBenchmark.getPrecision(), 0.001);
+        assertEquals(0.6, queryBenchmark.getPrecision(), 0.001);
         assertEquals(1, queryBenchmark.getRecall(), 0.001);
+    }
+
+    @Test
+    public void testPrecisionAndRecall2() throws Exception {
+        SearchResults expectedSearchResults = new SearchResults();
+        expectedSearchResults.addHit(1, 1);
+        expectedSearchResults.addHit(1, 2);
+        expectedSearchResults.addHit(1, 3);
+        expectedSearchResults.addHit(1, 4);
+        expectedSearchResults.addHit(1, 5);
+        expectedSearchResults.addHit(1, 6);
+        expectedSearchResults.addHit(1, 7);
+        expectedSearchResults.addHit(1, 8);
+        expectedSearchResults.addHit(1, 9);
+        expectedSearchResults.addHit(1, 10);
+        SearchResults actualSearchResults = new SearchResults();
+        actualSearchResults.addHit(1, 1);
+        actualSearchResults.addHit(1, 2);
+        actualSearchResults.addHit(1, 3);
+        actualSearchResults.addHit(1, 4);
+        actualSearchResults.addHit(1, 5);
+        actualSearchResults.addHit(1, 6);
+        QueryBenchmark queryBenchmark = new QueryBenchmark();
+        queryBenchmark.setExpectedSearchResults(expectedSearchResults);
+        queryBenchmark.setActualSearchResults(actualSearchResults);
+        assertEquals(1, queryBenchmark.getPrecision(), 0.001);
+        assertEquals(0.6, queryBenchmark.getRecall(), 0.001);
     }
 }
